3f2dbc84b7bfdc6f10ba38a334f3e7f8bfbe7998
wrecage <121277075+Wrecage@users.noreply.github.com>
2024-10-31 16:18:17 +0800

    Update .gitignore and remove tracked files

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
 import os
 import dj_database_url
+
+config = Config(search_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
